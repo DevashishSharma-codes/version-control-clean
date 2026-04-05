6ae062c0a41ecd2ab802b7bd37350d4ce63b07e3
Devashish Sharma <devashish2157@gmail.com>
2026-04-05 16:35:14 +0530

    Redis error fixed

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -3,30 +3,16 @@ require("dotenv").config();
 
 const client = redis.createClient({
   url: process.env.REDIS_URL,
-  socket: {
-    tls: true,
-    rejectUnauthorized: false,
-  },
 });
 
 client.on("error", (err) => {
-  console.error("Redis Client Error:", err);
+  console.error("Redis Client Error", err);
 });
 
 client.on("connect", () => {
   console.log("Redis connected...");
 });
 
-client.on("ready", () => {
-  console.log("Redis is ready to use");
-});
-
-(async () => {
-  try {
-    await client.connect();
-  } catch (err) {
-    console.error("Redis connection failed:", err);
-  }
-})();
+client.connect().catch(console.error);
 
 module.exports = client;
